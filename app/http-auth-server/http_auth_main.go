74c486dee70db56513129bd507357dad171e08fb
Yichen_Wu <804541459@qq.com>
2024-11-01 15:11:02 +0800

    feat:<http-auth-server>开发中...临时保存

--- app/http-auth-server/http_auth_main.go
+++ app/http-auth-server/http_auth_main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/SIN5t/tRPC-go/app/http-auth-server/service"
+	"trpc.group/trpc-go/trpc-go"
+	"trpc.group/trpc-go/trpc-go/log"
+)
+
+func main() {
+	s := trpc.NewServer()
+	if err := service.RegisterHttpAuthService(s); err != nil {
+		log.Fatal("服务初始化失败: %v", err)
+	}
+	if err := s.Serve(); err != nil {
+		log.Fatal("服务开启失败： %v", err)
+	}
+
+}
--- app/http-auth-server/service/http_auth_service.go
+++ app/http-auth-server/service/http_auth_service.go
@@ -1,1 +1,37 @@
 package service
+
+import (
+	"context"
+	"fmt"
+	"github.com/SIN5t/tRPC-go/proto/http_auth"
+	"trpc.group/trpc-go/trpc-go/server"
+)
+
+// 服务注册
+type httpAuthImpl struct {
+}
+
+func newHttpAuthImpl() httpAuthImpl {
+	return httpAuthImpl{}
+}
+
+func RegisterHttpAuthService(s server.Service) error {
+	httpImpl := newHttpAuthImpl()
+	http_auth.RegisterAuthService(s, httpImpl)
+	return nil
+}
+
+func (httpAuthImpl) Login(ctx context.Context, req *http_auth.LoginRequest) (*http_auth.LoginResponse, error) {
+	context.WithValue(ctx, "loginTest", "value")
+	username, pwd := req.GetUsername(), req.GetPasswordHash()
+	traceId := req.GetMataData().GetTraceId()
+	fmt.Println(username)
+	fmt.Println(pwd)
+
+	loginResponse := &http_auth.LoginResponse{
+		ErrCode: 0,
+		ErrMsg:  "",
+		Data:    &http_auth.LoginResponse_Data{IdTicket: traceId},
+	}
+	return loginResponse, nil
+}
